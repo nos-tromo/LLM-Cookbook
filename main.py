b6220ee9358bb68f1a42919ce153274e1d1d52e7
nos-tromo <ohhimarc@proton.me>
2024-08-11 22:01:42 +0200

    Initial project files

---
+++ main.py
@@ -0,0 +1,45 @@
+from openai import OpenAI
+import pandas as pd
+
+
+# load data and create prompt
+instruction = "Summarize the following text: {text}"
+df = pd.read_csv('data/data.txt', sep='\t', encoding='utf-8')
+text = ''.join(df.iloc[:, 2])
+prompt = instruction.format(text=text)
+
+# Point to the local server
+client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
+
+history = [
+    {"role": "system", "content": "You are an intelligent assistant. You always provide well-reasoned answers that are both correct and helpful."},
+    {"role": "user", "content": prompt},
+]
+
+while True:
+    completion = client.chat.completions.create(
+        model="lmstudio-community/gemma-2-9b-it-GGUF",
+        messages=history,
+        temperature=0.7,
+        stream=True,
+    )
+
+    new_message = {"role": "assistant", "content": ""}
+
+    for chunk in completion:
+        if chunk.choices[0].delta.content:
+            print(chunk.choices[0].delta.content, end="", flush=True)
+            new_message["content"] += chunk.choices[0].delta.content
+
+    history.append(new_message)
+
+    # Uncomment to see chat history
+    # import json
+    # gray_color = "\033[90m"
+    # reset_color = "\033[0m"
+    # print(f"{gray_color}\n{'-'*20} History dump {'-'*20}\n")
+    # print(json.dumps(history, indent=2))
+    # print(f"\n{'-'*55}\n{reset_color}")
+
+    print()
+    history.append({"role": "user", "content": input("> ")})
